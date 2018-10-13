68486a7801685db8123f00a8a271ab0767871a4f
wangjiliang <845506513@qq.com>
2018-10-13 22:54:02 +0800

    eureka联调

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,18 +1,54 @@
 package com.example.demo.Controller;
 
-import com.example.demo.Vo.Book;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
-    @Autowired
-    private Book book;
+    private final Logger logger = Logger.getLogger(getClass());
 
-    @RequestMapping(value = "/hello")
-    public String hello(){
-        return book.getDesc();
+    @Autowired
+    private DiscoveryClient discoveryClient; // 服务注册
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index(){
+        return "Hello World!";
     }
+
+    @RequestMapping("/registered")
+    public String getRegistered(){
+        List<ServiceInstance> list = discoveryClient.getInstances("STORES");
+        System.out.println(discoveryClient.getLocalServiceInstance());
+        System.out.println("discoveryClient.getServices().size() = " + discoveryClient.getServices().size());
+
+        for( String s :  discoveryClient.getServices()){
+            System.out.println("services " + s);
+            List<ServiceInstance> serviceInstances =  discoveryClient.getInstances(s);
+            for(ServiceInstance si : serviceInstances){
+                System.out.println("    services:" + s + ":getHost()=" + si.getHost());
+                System.out.println("    services:" + s + ":getPort()=" + si.getPort());
+                System.out.println("    services:" + s + ":getServiceId()=" + si.getServiceId());
+                System.out.println("    services:" + s + ":getUri()=" + si.getUri());
+                System.out.println("    services:" + s + ":getMetadata()=" + si.getMetadata());
+            }
+
+        }
+
+        System.out.println(list.size());
+        if (list != null && list.size() > 0 ) {
+            System.out.println( list.get(0).getUri()  );
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.Vo.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ac = SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
