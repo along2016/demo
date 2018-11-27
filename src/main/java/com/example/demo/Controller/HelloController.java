1ea8ef5e920f84b2ec65e549b0f9919df79a67c0
dtwangjiliang <845506513@qq.com>
2018-11-27 14:32:41 +0800

    去除log

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,11 +1,8 @@
 package com.example.demo.Controller;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +11,6 @@ import java.util.List;
 
 @RestController
 public class HelloController {
-
-    private final Logger logger = Logger.getLogger(getClass());
 
     @Autowired
     private DiscoveryClient discoveryClient; // 服务注册
