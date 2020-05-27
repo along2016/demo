eef1979c983a90572013e119eb40d6c4dc1b1812
dtwangjiliang <845506513@qq.com>
2020-05-27 13:53:50 +0800

    去除log

--- src/test/java/com/example/util/EnumTest.java
+++ src/test/java/com/example/util/EnumTest.java
@@ -9,12 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.Collection;
 import java.util.EnumMap;
+import java.util.EnumSet;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class)
 @WebAppConfiguration
-public class EnumMapTest {
+public class EnumTest {
 
     private EnumMap<Season, String> seasons = new EnumMap<>(Season.class);
 
@@ -34,8 +36,26 @@ public class EnumMapTest {
     }
 
     @Test
-    public void test(){
+    public void testEnumMap(){
         Assert.assertEquals(seasons.size(), 4);
 
+        Collection<String> values = seasons.values();
+        Assert.assertTrue(values.contains("寒风凛冽"));
+
+        Assert.assertTrue(seasons.keySet().contains(Season.FALL));
+    }
+
+    @Test
+    public void testEnumSet(){
+        EnumSet enumSet = EnumSet.allOf(Season.class);
+        System.out.println(enumSet);
+
+        EnumSet rangeEnumSet = EnumSet.range(Season.SPRING, Season.FALL);
+        System.out.println(rangeEnumSet);
+
+        EnumSet ofEnumSet = EnumSet.of(Season.SPRING, Season.WINTER);
+        System.out.println(ofEnumSet);
+
+        System.out.println(EnumSet.complementOf(ofEnumSet));
     }
 }
