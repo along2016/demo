2092ef674ca187a4b4e02059660df402c7a397f2
dtwangjiliang <845506513@qq.com>
2020-06-03 17:16:15 +0800

    去除log

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,67 +1,25 @@
 package com.example.demo;
 
-import com.example.demo.Controller.HelloController;
-import com.example.demo.Vo.Book;
-import org.junit.Before;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.StringUtils;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.util.TreeSet;
-
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
 public class DemoApplicationTests {
 
-    private MockMvc mvc;
-
-    @Autowired
-    protected WebApplicationContext wac;
-
-    @Before
-    public void setUp(){
-        mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-    }
+    @ApolloConfig
+    private Config config;
 
     @Test
-    public void hello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World!")));
-    }
-
-    /**
-     * 字符串反转
-     * @param str
-     * @return
-     */
-    private static String reserve(String str){
-        if(StringUtils.isEmpty(str)){
-            return str;
-        }
-        return reserve(str.substring(1)) + str.charAt(0);
-    }
-
-    @Test
-    public void testReserve(){
-        System.out.println(reserve("1234567"));
-//        TreeSet
+    public void hello(){
+        System.setProperty("env", "DEV");
+        System.out.println(config.getProperty("username", "admin"));
     }
 }
